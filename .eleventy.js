45a3de2df51551bff01cf5b529d2f1190fc4b8bb
JackSheepe <113531456+JackSheepe@users.noreply.github.com>
2022-10-27 16:06:59 +0700

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("src/styles/");
+  eleventyConfig.addPassthroughCopy("src/scripts/");
+  eleventyConfig.addPassthroughCopy("src/services/");
+  eleventyConfig.addPassthroughCopy("src/slides/");
+  eleventyConfig.addPassthroughCopy("src/portfolio_lists");
+  eleventyConfig.addPassthroughCopy("src/polygraphy/");
+  eleventyConfig.addPassthroughCopy("src/aboutUs.html");
+  eleventyConfig.addPassthroughCopy("src/contacts.html");
+  eleventyConfig.addPassthroughCopy("src/forClient.html");
+  eleventyConfig.addPassthroughCopy("src/paymentAndDelivery.html");
+  eleventyConfig.addPassthroughCopy("src/portfolio.html");
+  eleventyConfig.addPassthroughCopy("src/services.html");
+  eleventyConfig.addPassthroughCopy("src/outsideAd.html");
+  eleventyConfig.addPassthroughCopy("src/polygraphy.html");
+  eleventyConfig.addPassthroughCopy("src/adPlacement.html");
+
+  // Return your Object options:
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  };
+};
